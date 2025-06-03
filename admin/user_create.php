38efa1f747a40bb0f3d43405b4c65e6cf9804ea4
Andrii <andrii.kulyk.job@gmail.com>
2025-06-03 12:11:36 +0300

    fix(user_create): hash password before storing in database

--- admin/user_create.php
+++ admin/user_create.php
@@ -6,7 +6,7 @@ require_login();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $password = $_POST['password'];
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
   $role = $_POST['role'];
 
   $stmt = $pdo->prepare("INSERT INTO users (name, email, password, role) VALUES (?,?,?,?)");
