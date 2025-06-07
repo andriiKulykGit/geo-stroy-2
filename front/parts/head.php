5612756ebed5c1fa36f78bd5c23b5a543d3eabdb
Andrii <andrii.kulyk.job@gmail.com>
2025-06-07 17:39:51 +0300

    feat(head): add robots meta tag to prevent indexing

--- front/parts/head.php
+++ front/parts/head.php
@@ -2,6 +2,7 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
+  <meta name="robots" content="noindex">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>GeoStroy</title>
   <meta name="theme-color" content="#fff">
